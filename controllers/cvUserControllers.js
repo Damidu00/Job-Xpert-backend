f9a9a0d0cc0b2881d86003029d0e8915e5400b28
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-03-13 01:56:59 +0530

    cvuser create function created

---
+++ controllers/cvUserControllers.js
@@ -0,0 +1,19 @@
+import cvUsers from "../models/cvUserModel";
+
+export async function createCvUserDetails(req,res){
+    try {
+        const newUserdetails = req.body;
+        const user = new cvUsers(newUserdetails)
+        await user.save();
+
+        res.json({
+            message : "user details saved successfully"
+        })
+
+    } catch (error) {
+        res.json({
+            message : "error to save user",
+            error : error.message
+        })
+    }
+}
---
+++ routes/cvUserRoutes.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const cvUserRoutes = express.Router()
+
+cvUserRoutes
